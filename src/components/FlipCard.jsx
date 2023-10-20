85b2ccd239ee9768099e62bee91b9ad455088f39
SantiCombina <santicombina@gmail.com>
2023-10-20 13:37:07 -0300

    Changed  h1 to h3 semantic

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -22,9 +22,9 @@ export const FlipCard = ({ name, image, id, toggleCard, setToggleCard }) => {
           />
         </div>
         <div className="bg-gray-200 dark:bg-gray-900 text-black dark:text-white transition-all duration-700 absolute inset-0 h-full w-full rounded-xl px-12 text-center [transform:rotateY(-180deg)] [backface-visibility:hidden]">
-          <h1 className="flex min-h-full flex-col items-center justify-center text-xl font-semibold">
+          <h3 className="flex min-h-full flex-col items-center justify-center text-xl font-semibold">
             {name}
-          </h1>
+          </h3>
         </div>
       </div>
     </div>
